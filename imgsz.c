807e7dbb0bb9e5c31261c2c31516880fb870623d
itouhiro <itouhiro@gmail.com>
2001-09-18 22:33:08 +0900

    version 0.2

--- imgsz.c
+++ imgsz.c
@@ -2,39 +2,11 @@
  * imgsz  -  output imagesize-info as HTML format to stdout
  *
  * Filename:   imgsz.c
- * Version:    0.1
- * Time-stamp: <Sep 17 2001>
- * Author:     ITOU Hiroki (itouh@lycos.ne.jp)
+ * Version:    0.2
+ * Time-stamp: <Sep 18 2001>
  */
-
-/*
- * Copyright (c) 2001 ITOU Hiroki
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #define PROGNAME "imgsz"
-#define PROGVERSION "0.1"
+#define PROGVERSION "0.2"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,8 +15,8 @@
 
 #define SUFLEN 4 //suffix length (including dot)
                  //拡張子の文字数(dotも含める) ".jpg", ".png" など4文字
-#define GIFHEAD 32 //in fact, 10(sig(6) + width(2) + height(2) is enough
-#define PNGHEAD 32 //really, 24(sig(8) + IHDR len(4)+type(4)+width(4)+height(4)) is enough
+#define GIFHEAD 32 //in fact, 10 (sig(6)+width(2)+height(2)) is enough
+#define PNGHEAD 32 //really, 24 (sig(8)+IHDRlen(4)+type(4)+width(4)+height(4)) is enough
 #define XBMHEAD 100 //??
 #define MAXLINECHAR 1024
 #define errret { size.width = -1; return size; }
@@ -74,7 +46,9 @@ size_type xbmGetSize(char *fname);
 int main(int argc, char *argv[]){
 	int i;
 
-	//print help & exit
+	/*
+	 * print help & exit
+	 */
 	if(argc<=1){
 		fprintf(stderr,
 			"%s version %s\n"
@@ -91,12 +65,16 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	//argument handling
+	/*
+	 * argument handling
+	 */
 	for(i=1; i<argc; i++){
 		size_type size;
 		int ftype;
 
-		//filename check
+		/*
+		 * filename check
+		 */
 		ftype = getSuffix(argv[i]);
 		switch(ftype){
 		case 0:
@@ -116,7 +94,9 @@ int main(int argc, char *argv[]){
 			break;
 		}
 
-		//print to stdout
+		/*
+		 * print to stdout
+		 */
 		if(size.width==0){
 			; //not image-file suffix
 		}else if(size.width==-1){
@@ -155,22 +135,35 @@ int getSuffix(char *fname){
 
 
 size_type gifGetSize(char *fname){
-	//License:
-	//  The Graphics Interchange Format(c) is the Copyright property of
-	//  CompuServe Incorporated. GIF(sm) is a Service Mark property of
-	//  CompuServe Incorporated.
+	/*
+	 * License:
+	 *   The Graphics Interchange Format(c) is the Copyright property of
+	 *   CompuServe Incorporated. GIF(sm) is a Service Mark property of
+	 *   CompuServe Incorporated.
+	 */
 	size_type size;
 	unsigned char *p, src[GIFHEAD];
 	short w, h;
 	FILE *fin;
 
-	//file open
-	if( (fin=fopen(fname,"rb")) == NULL )
+	/*
+	 * file open
+	 */
+	if((fin=fopen(fname,"rb")) == NULL)
 		errret;
 	if(fread(src, 1, GIFHEAD, fin)!=GIFHEAD)
 		errret;
 
-	//get size (Logical Screen Size)
+	/*
+	 * validity check ファイル正当性チェック
+	 *  最初の6バイトが 'GIF87a' か 'GIF89a'であればいい
+	 */
+	if(memcmp(src, "GIF8", 4)!=0)
+		errret;
+
+	/*
+	 * get size (Logical Screen Size)
+	 */
 	p = src + 6; // 'GIF'(3) + version(3)
 	w = *p | *(p+1)<<8; //LSBfirst(little endian 2bytes)
 	h = *(p+2) | *(p+3)<<8;
@@ -188,37 +181,96 @@ size_type jpgGetSize(char *fname){
 	FILE *fin;
 	struct stat info;
 
-	//file open & memory allocate
+	/*
+	 * file open & memory allocate
+	 */
 	if((fin=fopen(fname,"rb")) == NULL)
 		errret;
 	if(stat(fname, &info) != 0)
 		errret;
 	if((src=malloc(info.st_size)) == NULL)
 		errret;
-	if(fread(src, 1, info.st_size, fin) != info.st_size)
+	if(fread(src, 1, info.st_size, fin) != info.st_size){
+		free(src);
 		errret;
+	}
+	fclose(fin);
 
-	//get size
+
+	/*
+	 * validity check
+	 *  最初の2バイトは SOI というmarker。
+	 *  marker とは、各セグメント最初の2バイトにある識別子。
+	 *  PNGでいうchunkname。
+	 *
+	 *  JFIFの場合は APP0 も調べたほうがいいけど調べてない。
+	 */
+	if(*src==0xff && *(src+1)==0xd8){
+		;//SOI(start of image) exists
+	}else{
+		free(src);
+		errret;
+	}
+
+	/*
+	 * get size
+	 */
 	p = src + 2; //SOI(start of image)(2bytes)
 	for(;;){
-		short next;
-		if(*p==0xff && *(p+1)==0xc0){
-			//found SOF0 (start of frame 0)
-			p += 5; //marker(2) + segmentlength(2) + bit/sample(1)
-			h = *p<<8 | *(p+1);
-			w = *(p+2)<<8 | *(p+3);
-			size.width = w;
-			size.height = h;
-			return size;
+		short next; //次のセグメントまでの相対距離
+		static unsigned char sof[] = { 0xc0, 0xc1, 0xc2, 0xc3,
+					       0xc5, 0xc6, 0xc7, 0xc9,
+					       0xca, 0xcb, 0xcd, 0xce,
+					       0xcf, };
+		//0xc4,0xc8,0xccは、SOFn以外に使用されている
+		int i;
+
+		//セグメント先頭の 'ff'をサーチ
+		while(*p!=0xff){
+			p++;
+			if(p >= src + info.st_size){
+				free(src);
+				errret;
+			}
 		}
-		p += 2; //marker(2bytes PNGでいう chunkname)をとばすのだが、
-		//segment lengthに markerも含まれている
-		next = *p<<8 | *(p+1); //このセグメントをとばす
-		p = p + next;
-		if(p >= src+info.st_size)
+
+		//セグメント種を調べる
+		for(i=0; i<sizeof(sof)/sizeof(sof[0]); i++){
+			if(*(p+1) == sof[i]){
+				//SOFn (start of frame) を見つけた
+				p += 5; //marker(2) + segmentlength(2) + bit/sample(1)
+				if(p >= src + info.st_size){
+					free(src);
+					errret;
+				}
+				h = *p<<8 | *(p+1);
+				w = *(p+2)<<8 | *(p+3);
+				size.width = w;
+				size.height = h;
+				free(src);
+				return size;
+			}
+		}
+		if(*(p+1)==0xd8 || *(p+1)==0xd3){
+			//EOI(end of image), SOS(start of scan) が
+			//SOFnの前にある => なんらかのエラー
+			free(src);
 			errret;
-	}
-	errret;
+		}else{
+			//ここで探してない種のmarkerだった
+			p += 2;
+			if(p >= src + info.st_size){
+				free(src);
+				errret;
+			}
+			next = *p<<8 | *(p+1);
+			p = p + next; //skip this segment
+			if(p >= src + info.st_size){
+				free(src);
+				errret;
+			}
+		}//if(*p
+	}//for(;;
 }
 
 
@@ -228,14 +280,27 @@ size_type pngGetSize(char *fname){
 	long w, h;
 	FILE *fin;
 
-	//file open
-	if( (fin=fopen(fname,"rb")) == NULL )
+	/*
+	 * file open
+	 */
+	if((fin=fopen(fname,"rb")) == NULL)
 		errret;
 	if(fread(src, 1, PNGHEAD, fin)!=PNGHEAD)
 		errret;
 
+	/*
+	 * validity check
+	 *  最初の8バイトが、89 50 4e 47 0d 0a 1a 0a
+	 *                  (c) P  N  G  \r \n    \n
+	 */
+	if(memcmp(src, "\x89PNG\r\n\x1a\x0a", 8)==0)
+		;
+	else
+		errret;
 
-	//get size
+	/*
+	 * get size
+	 */
 	p = src + 16; //PNG signature(8) + IHDR chunk length(4)+type(4)
 	w = *p<<24 | *(p+1)<<16 | *(p+2)<<8 | *(p+3); //MSBfirst(big endian 4bytes)
 	h = *(p+4)<<24 | *(p+5)<<16 | *(p+6)<<8 | *(p+7);
@@ -250,14 +315,18 @@ size_type xbmGetSize(char *fname){
 	size_type size;
 	FILE *fin;
 	char *p, *top, src[MAXLINECHAR];
-	long w, h;
+	long w=0, h=0;
 	int flag = 0;
 
-	//file open
+	/*
+	 * file open
+	 */
 	if( (fin=fopen(fname,"r")) == NULL )
 		errret;
 
-	//get size
+	/*
+	 * get size
+	 */
 	for(;;){
 		if(flag>=2)
 			break;
