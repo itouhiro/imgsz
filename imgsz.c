066044d5afa3309a3301213a833bd780753b4145
itouhiro <itouhiro@gmail.com>
2009-06-11 16:07:26 +0900

    version 0.6
    output text or XHTML format
    add option '--altfilename', '--normal'
    handle upper case extensions like 'GIF', 'JPG'
    'imgsz.c' encode change to 'UTF-8'

--- imgsz.c
+++ imgsz.c
@@ -1,14 +1,18 @@
 /*
- * imgsz  -  print width & height sizes of images as HTML format
+ * imgsz  -  print 'image size' -- print width and height pixels as XHTML format
  *
  * Filename:   imgsz.c
- * Version:    0.5
+ * Version:    0.6
  * Author:     itouh
- * Time-stamp: <Jan 20 2009>
+ * Time-stamp: <Jun 11 2009>
  * Copyright (c) 2003,2009 Itou Hiroki
+ *
+ * todo: JPEG file may cause error,  because of reading first 500 kilo bytes only of a JPEG fiile.
+ *       JPEG„Éï„Ç°„Ç§„É´„ÅØÔºà„Éï„Ç°„Ç§„É´Ë™≠„ÅøËæº„Åø„ÅÆÈÄüÂ∫¶„ÅÆ„Åü„ÇÅÔºâÊúÄÂ§ß„Åß0.5MB„Åæ„Åß„Åó„ÅãË™≠„ÅøËæº„Åæ„Å™„ÅÑ„ÅÆ„Åß„ÄÅ„Ç®„É©„Éº„Å´„Å™„Çã„Åã„ÇÇ„ÄÇ
+ *       GIF,PNG„ÅØÂÖàÈ†≠32„Éê„Ç§„Éà„Åó„ÅãË™≠„ÅøËæº„Åæ„Å™„ÅÑ„ÅÆ„ÅßÊó©„ÅÑ„ÅóÂïèÈ°å„Å™„ÅÑ„ÄÇ
  */
 #define PROGNAME "imgsz"
-#define PROGVERSION "0.5"
+#define PROGVERSION "0.6"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,12 +23,13 @@
 #define PNGHEAD 32 //24 byte (sig(8)+IHDRlen(4)+type(4)+width(4)+height(4)) is enough
 #define XBMHEAD 100 //??
 #define MAXLINECHAR 1024
-#define errret {free(src); size.width=-1; return size;}
+#define MAXFILEBYTE (1024*512)
+#define errret {free(src); pxsize.width=-1; return pxsize;}
 
 /*
  * global variable
  */
-const char *g_suffix[] = { ".gif", ".jpg", ".png", ".xbm" };
+const char *g_suffix[] = { ".gif", ".GIF", ".jpg", ".jpeg",".JPG",".JPEG",".png", ".PNG",".xbm", ".XBM" };
 char g_exifdate[MAXLINECHAR];
 char g_exifmaker[MAXLINECHAR];
 char g_exifmodel[MAXLINECHAR];
@@ -36,7 +41,7 @@ typedef struct{
     long width;
     long height;
 } size_type;
-typedef enum {gif, jpg, png, xbm, notype} imagetype;
+typedef enum {gif, GIF, jpg, jpeg, JPG, JPEG, png, PNG, xbm, XBM, notype} imagetype;
 
 
 /*
@@ -51,6 +56,7 @@ void getExifInfo(unsigned char *exiftop);
 void getExifData(unsigned char *dst, unsigned char *p, unsigned char *exiftop, int bigEndian);
 unsigned short big2ltls(void *v);
 unsigned long big2ltll(void *v);
+char * getBasename(char * str);
 
 /***********************************************************************/
 int main(int argc, char *argv[]){
@@ -58,6 +64,8 @@ int main(int argc, char *argv[]){
     int ratio = 1;
     int exif = 0;
     int basename = 0;
+    int xhtml_output = 1;
+    int alt_filename = 0;
 
     /*
      * print help & exit
@@ -66,14 +74,15 @@ int main(int argc, char *argv[]){
         printf("%s version %s - print width & height sizes of images as XHTML format\n"
                "usage:     %s <option> file..\n"
                "option:    -2  --double      double size for width, height\n"
-               "           -e  --exif        put Exif info if exist\n"
-               "           -b  --basename    remove prefix path and show just an image file name\n"
+               "           -a  --altfilename 'alt' attribute with filename without suffix\n"
+               "           -b  --basename    remove prefix path and show just a filename\n"
+               "           -e  --exif        output Exif info if exist.  EXCLUSIVE with '-a'\n"
+               "           -n  --normal      no XHTML output.  format: filename width height\n"
                "supported: ", PROGNAME, PROGVERSION, PROGNAME);
         for(i=0; i<sizeof(g_suffix)/sizeof(g_suffix[0]); i++)
             fprintf(stdout, "%s ", g_suffix[i]);
         fflush(stdout);
-        printf("\n"
-               "  (not support upper-case 'JPG', 'PNG'..)\n");
+        printf("\n");
         exit(1);
     }
 
@@ -81,21 +90,29 @@ int main(int argc, char *argv[]){
      * argument handling
      */
     for(i=1; i<argc; i++){
-        size_type size;
+        size_type pxsize; //pixel size
         int ftype;
-        char *img_fname, *p, *j;
+        char *img_fname, *p;
 
         if((strcmp(argv[i],"-2")==0) ||
            (strcmp(argv[i],"--double")==0)){
             ratio = 2;
             continue;
-        }else if((strcmp(argv[i],"-e")==0) ||
-                 (strcmp(argv[i],"--exif")==0)){
-            exif = 1;
+        }else if((strcmp(argv[i],"-a")==0) ||
+                 (strcmp(argv[i],"--altfilename")==0)){
+            alt_filename = 1;
             continue;
         }else if((strcmp(argv[i],"-b")==0) ||
                  (strcmp(argv[i],"--basename")==0)){
             basename = 1;
+            continue;
+        }else if((strcmp(argv[i],"-e")==0) ||
+                 (strcmp(argv[i],"--exif")==0)){
+            exif = 1;
+            continue;
+        }else if((strcmp(argv[i],"-n")==0) ||
+                 (strcmp(argv[i],"--normal")==0)){
+            xhtml_output = 0;
             continue;
         }
 
@@ -105,19 +122,25 @@ int main(int argc, char *argv[]){
         ftype = getSuffix(argv[i]);
         switch(ftype){
         case 0:
-            size = gifGetSize(argv[i]); //gif
-            break;
         case 1:
-            size = jpgGetSize(argv[i]); //jpg
+            pxsize = gifGetSize(argv[i]); //gif
             break;
         case 2:
-            size = pngGetSize(argv[i]); //png
-            break;
         case 3:
-            size = xbmGetSize(argv[i]); //xbm
+        case 4:
+        case 5:
+            pxsize = jpgGetSize(argv[i]); //jpg
+            break;
+        case 6:
+        case 7:
+            pxsize = pngGetSize(argv[i]); //png
+            break;
+        case 8:
+        case 9:
+            pxsize = xbmGetSize(argv[i]); //xbm
             break;
         default:
-            size.width = -1;
+            pxsize.width = -1;
             break;
         }
 
@@ -127,31 +150,34 @@ int main(int argc, char *argv[]){
         img_fname = (char *)malloc(MAXLINECHAR);
         memset(img_fname, 0x00, MAXLINECHAR);
         p=argv[i];
-        //for (j=p+strlen(argv[i])+1; j>=p; j--){
-        //    if(*j=='\n' || *j=='\r') *j='\0';
-        //}
-        if (basename){
-            //printf("debug %p %d\n", p, strlen(argv[i]));
-            for (j=argv[i]; j<argv[i]+strlen(argv[i]) - 1; j++){
-                if (*j =='\\' || *j =='/'){
-                    p = j+1;
-                    //printf("debug %s (%d)\n", p, j-argv[0]);
-                }
-            }
-        }
+        if (basename){ p = getBasename(argv[i]); }
         strncpy(img_fname, p, MAXLINECHAR - 1);
 
-        if(size.width <= -1){
-            fprintf(stdout, "<!-- error: '%s' is not an image file? -->\n", argv[i]);
+        if(pxsize.width <= -1){
+            fprintf(stdout, "<!-- error: '%s' is not an image file? or JPEG MetaInfo is over 1MB. -->\n", argv[i]);
         }else{
-            if(exif && *g_exifdate!='\0'){
-                //fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"%s %s %s\" title=\"%s %s %s\" />\n", argv[i], size.width * ratio, size.height * ratio, g_exifmaker, g_exifmodel, g_exifdate, g_exifmaker, g_exifmodel, g_exifdate);
-                fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"%s %s\" title=\"%s %s\" />\n", img_fname, size.width * ratio, size.height * ratio, g_exifmodel, g_exifdate, g_exifmodel, g_exifdate);
-                memset(g_exifdate, 0x00, MAXLINECHAR);
-                memset(g_exifmaker, 0x00, MAXLINECHAR);
-                memset(g_exifmodel, 0x00, MAXLINECHAR);
-            }else{
-                fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"\" />\n", img_fname, size.width * ratio, size.height * ratio);
+            if(xhtml_output){
+                if(! alt_filename && exif && *g_exifdate!='\0'){
+                    //fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"%s %s %s\" title=\"%s %s %s\" />\n", argv[i], pxsize.width * ratio, pxsize.height * ratio, g_exifmaker, g_exifmodel, g_exifdate, g_exifmaker, g_exifmodel, g_exifdate);
+                    fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"%s %s\" title=\"%s %s\" />\n", img_fname, pxsize.width * ratio, pxsize.height * ratio, g_exifmodel, g_exifdate, g_exifmodel, g_exifdate);
+                    memset(g_exifdate, 0x00, MAXLINECHAR);
+                    memset(g_exifmaker, 0x00, MAXLINECHAR);
+                    memset(g_exifmodel, 0x00, MAXLINECHAR);
+                }else{
+                    char alt_str[MAXLINECHAR];
+                    char * begin_suffix;
+                    if(alt_filename){
+                        strncpy(alt_str, getBasename(img_fname), MAXLINECHAR - 1);
+                        //delete suffix
+                        begin_suffix = strrchr(alt_str, '.');
+                        if(begin_suffix != NULL){ *begin_suffix = '\0'; }
+                    }else{
+                        * alt_str = '\0';
+                    }
+                    fprintf(stdout, "<img src=\"%s\" width=\"%ld\" height=\"%ld\" alt=\"%s\" />\n", img_fname, pxsize.width * ratio, pxsize.height * ratio, alt_str);
+                }
+            }else{ //if(xhtml_output)
+                fprintf(stdout, "%s %ld %ld\n", img_fname, pxsize.width * ratio, pxsize.height * ratio);
             }
         }
     }//for(i
@@ -166,7 +192,7 @@ int getSuffix(char *fname){
 
     suf = strrchr(fname, '.');
     if(suf == NULL) return notype;
-    sufnum = sizeof(g_suffix) / sizeof(g_suffix[0]); //≥»ƒ•ª“§ŒºÔŒ‡§ŒøÙ
+    sufnum = sizeof(g_suffix) / sizeof(g_suffix[0]); //Êã°ÂºµÂ≠ê„ÅÆÁ®ÆÈ°û„ÅÆÊï∞
     for(i=0; i<sufnum; i++){
         if(strcmp(suf, g_suffix[i]) == 0)
             return i;
@@ -176,7 +202,7 @@ int getSuffix(char *fname){
 
 
 size_type jpgGetSize(char *fname){
-    size_type size;
+    size_type pxsize;
     unsigned char *p, *pmax;
     unsigned char *src = NULL;
     int flagsize = 0;
@@ -185,21 +211,26 @@ size_type jpgGetSize(char *fname){
     {
         FILE *fin;
         struct stat info;
+        unsigned int filesize = 0;
 
         if((fin=fopen(fname,"rb")) == NULL) errret;
         if(stat(fname, &info) != 0) errret;
-        if((src=malloc(info.st_size)) == NULL) errret;
-        if(fread(src, 1, info.st_size, fin) != info.st_size){
+        filesize = info.st_size;
+        if(filesize > MAXFILEBYTE){
+            filesize = MAXFILEBYTE;
+        }
+        if((src=malloc(filesize)) == NULL) errret;
+        if(fread(src, 1, filesize, fin) != filesize){
             free(src); errret;
         }
         fclose(fin);
-        pmax = src + info.st_size;
+        pmax = src + filesize;
     }
 
     /*
-     * JPG ¬≈≈ˆ¿≠•¡•ß•√•Ø
-     *  ∫«ΩÈ§Œ 2byte §¨ SOI(start of image) §»§§§¶ marker
-     *  §»∞Ï√◊§π§Ï§– jpeg ≤Ë¡¸°£
+     * JPG Â¶•ÂΩìÊÄß„ÉÅ„Çß„ÉÉ„ÇØ
+     *  ÊúÄÂàù„ÅÆ 2byte „Åå SOI(start of image) „Å®„ÅÑ„ÅÜ marker
+     *  „Å®‰∏ÄËá¥„Åô„Çå„Å∞ jpeg ÁîªÂÉè„ÄÇ
      */
     if(*src==0xff && *(src+1)==0xd8){
         p = src + 2; //2 byte
@@ -208,59 +239,59 @@ size_type jpgGetSize(char *fname){
     }
 
 
-    /* SOFn •ª•∞•·•Û•»§Ú√µ§π */
+    /* SOFn „Çª„Ç∞„É°„É≥„Éà„ÇíÊé¢„Åô */
     for(;;){
         while(*p!=0xff){
             p++; if(p>=pmax) errret;
         }
 
-        //SOS(start of scan),EOI(end of image) •ª•∞•·•Û•»§Ú∏´§ƒ§±§ø§Œ§«•«°º•ø∏°∫˜§œΩ™Œª
+        //SOS(start of scan),EOI(end of image) „Çª„Ç∞„É°„É≥„Éà„ÇíË¶ã„Å§„Åë„Åü„ÅÆ„Åß„Éá„Éº„ÇøÊ§úÁ¥¢„ÅØÁµÇ‰∫Ü
         if((*(p+1)==0xda || *(p+1)==0xd9) && flagsize==1){
             free(src);
-            return size;
+            return pxsize;
         }
 
         //printf("ff %02x\n", *(p+1));
 
-        // SOFn •ª•∞•·•Û•»§´§…§¶§´•¡•ß•√•Ø
+        // SOFn „Çª„Ç∞„É°„É≥„Éà„Åã„Å©„ÅÜ„Åã„ÉÅ„Çß„ÉÉ„ÇØ
         if(flagsize==0){
             int i;
             static unsigned char sof[] = { 0xc0, 0xc1, 0xc2, 0xc3, 0xc5, 0xc6, 0xc7, 0xc9, 0xca, 0xcb, 0xcd, 0xce, 0xcf, };
             for(i=0; i<sizeof(sof)/sizeof(sof[0]); i++){
                 if(*(p+1) == sof[i]){
                     /*
-                     * SOFn •ª•∞•·•Û•»»Ø∏´
-                     * ≤Ë¡¸§Œ•‘•Ø•ª•Î•µ•§•∫§Ú∆¿§Î
+                     * SOFn „Çª„Ç∞„É°„É≥„ÉàÁô∫Ë¶ã
+                     * ÁîªÂÉè„ÅÆ„Éî„ÇØ„Çª„É´„Çµ„Ç§„Ç∫„ÇíÂæó„Çã
                      * 
                      * SOFmarker segmentlength bit/sample height width ..
                      * ff cN     NN NN         NN         NN NN  NN NN ..
                      */
-                    size.height = *(p+5)<<8 | *(p+6);
-                    size.width = *(p+7)<<8 | *(p+8);
-                    //printf("width=%ld height=%ld\n", size.width, size.height);
+                    pxsize.height = *(p+5)<<8 | *(p+6);
+                    pxsize.width = *(p+7)<<8 | *(p+8);
+                    //printf("width=%ld height=%ld\n", pxsize.width, pxsize.height);
                     flagsize = 1;
                     break;
                 }
             }//for
         }//if
 
-        //APPn •ª•∞•·•Û•»§´§…§¶§´•¡•ß•√•Ø
+        //APPn „Çª„Ç∞„É°„É≥„Éà„Åã„Å©„ÅÜ„Åã„ÉÅ„Çß„ÉÉ„ÇØ
         if(*(p+1)>=0xe0 && *(p+1)<0xef){
             if(strncmp(p+4, "Exif", strlen("Exif"))==0){
                 getExifInfo(p+2+2+6); //APP1marker(2byte) + APP1size(2) + APP1str(6)
             }
         }
 
-        //SOFn •ª•∞•·•Û•»§«§œ§ §´§√§ø
+        //SOFn „Çª„Ç∞„É°„É≥„Éà„Åß„ÅØ„Å™„Åã„Å£„Åü
         {
             unsigned short next = (*(p+2))<<8 | *(p+3);
             p += 2;
-            p += next; //marker(2byte) + segment length §Ú¬≠§π
+            p += next; //marker(2byte) + segment length „ÇíË∂≥„Åô
             if(p>=pmax) errret;
         }
     }
 
-    return size;
+    return pxsize;
 }
 
 
@@ -271,7 +302,7 @@ void getExifInfo(unsigned char *exiftop){
     unsigned char *exifsubifd = NULL;
     int i;
 
-    //endian •¡•ß•√•Ø
+    //endian „ÉÅ„Çß„ÉÉ„ÇØ
     if(*p=='M' && *(p+1)=='M'){
         bigEndian = 1; //Motorola(BigEndian)
     }else if(*p=='I' && *(p+1)=='I'){
@@ -302,7 +333,7 @@ void getExifInfo(unsigned char *exiftop){
             tag = *(unsigned short *)p;
         }
 
-        //…¨Õ◊§  Exif •«°º•ø§ÚºË∆¿§π§Î
+        //ÂøÖË¶Å„Å™ Exif „Éá„Éº„Çø„ÇíÂèñÂæó„Åô„Çã
         if(tag == 0x010f){
             //Make
             getExifData(g_exifmaker, p, exiftop, bigEndian);
@@ -313,7 +344,7 @@ void getExifInfo(unsigned char *exiftop){
             //DateTimeOriginal / DateTime
             getExifData(g_exifdate, p, exiftop, bigEndian);
         }else if(tag == 0x8769){
-            //ExifOffset §Œ•ø•∞§«§¢§Ï§– SubIFD§ÿ§Œ•™•’•ª•√•»§Úµ≠œø
+            //ExifOffset „ÅÆ„Çø„Ç∞„Åß„ÅÇ„Çå„Å∞ SubIFD„Å∏„ÅÆ„Ç™„Éï„Çª„ÉÉ„Éà„ÇíË®òÈå≤
             unsigned long offset;
             if(bigEndian){
                 offset = big2ltll(p+8);
@@ -378,7 +409,7 @@ size_type gifGetSize(char *fname){
      *   CompuServe Incorporated. GIF(sm) is a Service Mark property of
      *   CompuServe Incorporated.
      */
-    size_type size;
+    size_type pxsize;
     unsigned char *p, src[GIFHEAD];
     short w, h;
     FILE *fin;
@@ -393,8 +424,8 @@ size_type gifGetSize(char *fname){
     fclose(fin);
 
     /*
-     * validity check •’•°•§•Î¿µ≈ˆ¿≠•¡•ß•√•Ø
-     *  ∫«ΩÈ§Œ6•–•§•»§¨ 'GIF87a' §´ 'GIF89a'§«§¢§Ï§–§§§§
+     * validity check „Éï„Ç°„Ç§„É´Ê≠£ÂΩìÊÄß„ÉÅ„Çß„ÉÉ„ÇØ
+     *  ÊúÄÂàù„ÅÆ6„Éê„Ç§„Éà„Åå 'GIF87a' „Åã 'GIF89a'„Åß„ÅÇ„Çå„Å∞„ÅÑ„ÅÑ
      */
     if(memcmp(src, "GIF8", 4)!=0)
         errret;
@@ -406,14 +437,14 @@ size_type gifGetSize(char *fname){
     w = *p | *(p+1)<<8; //LSBfirst(little endian 2bytes)
     h = *(p+2) | *(p+3)<<8;
 
-    size.width = w;
-    size.height = h;
-    return size;
+    pxsize.width = w;
+    pxsize.height = h;
+    return pxsize;
 }
 
 
 size_type pngGetSize(char *fname){
-    size_type size;
+    size_type pxsize;
     unsigned char *p, src[PNGHEAD];
     long w, h;
     FILE *fin;
@@ -429,7 +460,7 @@ size_type pngGetSize(char *fname){
 
     /*
      * validity check
-     *  ∫«ΩÈ§Œ8•–•§•»§¨°¢89 50 4e 47 0d 0a 1a 0a
+     *  ÊúÄÂàù„ÅÆ8„Éê„Ç§„Éà„Åå„ÄÅ89 50 4e 47 0d 0a 1a 0a
      *                  (c) P  N  G  \r \n    \n
      */
     if(memcmp(src, "\x89PNG\r\n\x1a\x0a", 8)==0)
@@ -444,14 +475,14 @@ size_type pngGetSize(char *fname){
     w = *p<<24 | *(p+1)<<16 | *(p+2)<<8 | *(p+3); //MSBfirst(big endian 4bytes)
     h = *(p+4)<<24 | *(p+5)<<16 | *(p+6)<<8 | *(p+7);
 
-    size.width = w;
-    size.height = h;
-    return size;
+    pxsize.width = w;
+    pxsize.height = h;
+    return pxsize;
 }
 
 
 size_type xbmGetSize(char *fname){
-    size_type size;
+    size_type pxsize;
     FILE *fin;
     char *p, *top, src[MAXLINECHAR];
     long w=0, h=0;
@@ -478,11 +509,11 @@ size_type xbmGetSize(char *fname){
             errret;
         }
 
-        //π‘Àˆ§ﬁ§«ƒ¥§Ÿ§Î
+        //Ë°åÊú´„Åæ„ÅßË™ø„Åπ„Çã
         while(*p!='\0'){
             if(strncmp(p, "width", strlen("width"))==0){
-                top = memchr(p, ' ', XBMHEAD); //top§Ú width§Œ§¢§»§Œ∫«ΩÈ§Œ∂ı«Ú§À∞Ã√÷
-                p = memchr(top, '\n', XBMHEAD); //p§Úwidth§Œπ‘§Œ≤˛π‘∞Ã√÷§À∞Ã√÷,\0 §«§¶§·§Î
+                top = memchr(p, ' ', XBMHEAD); //top„Çí width„ÅÆ„ÅÇ„Å®„ÅÆÊúÄÂàù„ÅÆÁ©∫ÁôΩ„Å´‰ΩçÁΩÆ
+                p = memchr(top, '\n', XBMHEAD); //p„Çíwidth„ÅÆË°å„ÅÆÊîπË°å‰ΩçÁΩÆ„Å´‰ΩçÁΩÆ,\0 „Åß„ÅÜ„ÇÅ„Çã
                 *p = '\0';
                 w = atoi(top);
                 flag++;
@@ -494,13 +525,13 @@ size_type xbmGetSize(char *fname){
                 flag++;
             }//if(strncmp
             p++;
-        }//while(*p   1π‘§÷§Û§Œ•¡•ß•√•Ø§¨§™§Ô§√§ø
+        }//while(*p   1Ë°å„Å∂„Çì„ÅÆ„ÉÅ„Çß„ÉÉ„ÇØ„Åå„Åä„Çè„Å£„Åü
     }
     fclose(fin);
 
-    size.width = w;
-    size.height = h;
-    return size;
+    pxsize.width = w;
+    pxsize.height = h;
+    return pxsize;
 }
 /* exchange big endian to little endina; 16 bit short */
 unsigned short big2ltls(void *v){
@@ -522,5 +553,18 @@ unsigned long big2ltll(void *v){
         *(buf+i) = *(c + sizeof(unsigned long) - 1 - i);
     }
     return *(unsigned long*)buf;
+}
+
+char * getBasename(char * str){
+    char * p = str;
+    char * j;
+    //printf("debug %p %d\n", p, strlen(str));
+    for (j=str; j<str+strlen(str) - 1; j++){
+        if (*j =='\\' || *j =='/'){
+            p = j+1;
+            //printf("debug %s (%d)\n", p, j-argv[0]);
+        }
+    }
+    return p;
 }
 //end of file
